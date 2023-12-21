7b88bf092d191c3e3f1e840c982e17bb5cf39aa3
Muttaqeen Slamat <muttaqeens@gmail.com>
2023-12-21 18:59:50 +0200

    patch

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <main class="container-fluid">
+    <NavBar/>
+    <Footer/>
+    <router-view/>
+  </main>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script >
+import NavBar from './components/NavBar.vue';
+import Footer from './components/Footer.vue';
+export default{
+    component: {
+        NavBar,
+        Footer
+    },
+    components: { NavBar }
 }
+</script>
 
-nav {
-  padding: 30px;
-}
+<style src="@/assets/css/styles.css">
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
